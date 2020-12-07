fa1797d4c0a00dfabf5cfb3e6450481385388243
L1R1KA <70565141+L1R1KA@users.noreply.github.com>
2020-12-07 23:22:16 +0300

    Create Furniture.java

---
+++ Furniture.java
@@ -0,0 +1,61 @@
+package com.company;
+
+public abstract class Furniture {
+    private int price;
+    private int weight;
+    private int sizeX;
+    private int sizeY;
+    private int sizeZ;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public void setSizeX(int sizeX) {
+        this.sizeX = sizeX;
+    }
+
+    public int getSizeY() {
+        return sizeY;
+    }
+
+    public void setSizeY(int sizeY) {
+        this.sizeY = sizeY;
+    }
+
+    public int getSizeZ() {
+        return sizeZ;
+    }
+
+    public void setSizeZ(int sizeZ) {
+        this.sizeZ = sizeZ;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+
+}
+
